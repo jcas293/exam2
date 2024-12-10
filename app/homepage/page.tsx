7fcbd3ce2d018c133df794d0dba47c51f14b7bc0
jcas293 <jennifer.castillo48@qmail.cuny.edu>
2024-12-10 01:07:09 +0000

    homepage

--- app/homepage/page.tsx
+++ app/homepage/page.tsx
@@ -1,10 +1,14 @@
 
-export default function Home() {
+export default function Page() {
 
   return (
     <>
 
         <h1 className="text-center text-xl font-bold underline m-4 text-indigo-800"> Animal Crossing Guide </h1>
+        <p className='mx-10 my-2'>This website was created by me just because 
+          I really enjoy Animal Crossing New Horzions. I wanted to give tips and tricks to 
+          all those who are starting the game.</p>
+
         <p className='mx-10 my-2'>January 2025</p>
        
 
