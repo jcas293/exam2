8f19a8ab94b6103d5a3ac4a0a026f7ff6341398d
jcas293 <jennifer.castillo48@qmail.cuny.edu>
2024-12-10 00:58:28 +0000

    style

--- app/final/page.tsx
+++ app/final/page.tsx
@@ -1,13 +1,13 @@
 export default function Page(){
     return(
         <>
-        <h1 className="text-3xl"> Final Exam</h1>
+        <h1 className="text-3xl m-5 ml-5"> Final Exam</h1>
         <form> 
-         <label htmlFor="product">Product Description </label>
-         <input type="text" name="product" id="product" className= "text-black bg-blue-200" />
-        
+         <label htmlFor="product" className= "ml-5">Product Description </label>
+         <input type="text" name="product" id="product" className= "m-5 text-black bg-blue-200" />
+        <br />
        
-         <button type="button" className="text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Save</button>
+         <button type="button" className="ml-5 text-white bg-purple-700 hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Save</button>
         </form>
 
 
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ export default function Home() {
 
     return (
       <>
-        <h1 className="text-3xl"> Welcome to my Animal Crossing Guide!</h1>
+      <br />
+        <h1 className="m-5 text-3xl"> Welcome to my Animal Crossing Guide!</h1>
 
         <h1 className="text-center text-xl font-bold underline m-4 text-indigo-800"> Animal Crossing History</h1>
       <p className='mx-10 my-2'>The series of Animal Crossing has been around since 2001. Since then many versions for many different consoles have been released. The most recent adaptation of the game is Animal Crossing: New Horizons. In this game you take on the role of a character who moves to a deserted island after purhcasing a getaway package from Tom Nook, who is a recurring character throughout the series. Your goal is to develop the island into a thriving community by gathering resources, crafting items, decorating the landscape, and building homes. You interact with anthropomorphic animal villagers, participate in various seasonal events, and can visit other players islands via online multiplayer. The games open-ended nature allows for creativity and relaxation, with no strict objectives or time limits.</p>
