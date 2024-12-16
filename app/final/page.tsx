e9eb83103390e4d3a6f043721c36211512a2e396
jcas293 <jennifer.castillo48@qmail.cuny.edu>
2024-12-16 21:34:50 +0000

    final 1.2

--- app/final/page.tsx
+++ app/final/page.tsx
@@ -37,7 +37,7 @@ export default function Page(){
   <div className="md:flex md:items-center">
     <div className="md:w-1/3"></div>
     <div className="md:w-2/3">
-      <button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+      <button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="submit">
         Sign Up
       </button>
     </div>
