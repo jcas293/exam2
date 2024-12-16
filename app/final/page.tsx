a562414b4e0bbde4715d825e47081f496e37a486
jcas293 <jennifer.castillo48@qmail.cuny.edu>
2024-12-16 20:38:55 +0000

    form 1

--- app/final/page.tsx
+++ app/final/page.tsx
@@ -2,27 +2,73 @@ export default function Page(){
     return (
         <>
 
-        <h1 className="text-3xl m-5 ml-5"> Final Exam</h1>
-        <h2 className = "text-m m-5 ml-5 "> Fill out the form below if you want to be notified whenever a new post appears!</h2>
+        <h1 className = "text-3xl m-5 ml-5"> Final exam </h1>
 
-        <form> 
-        <label htmlFor="name" className="ml-5 m-5 ">
-        Name: 
-       <input type="text" className="input input-bordered input-primary input-sm w-full max-w-xs" placeholder="Enter your name" />
+     <form className="w-full max-w-sm">
+  <div className="md:flex md:items-center mb-6">
+    <div className="md:w-1/3">
+      <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-full-name">
+        Full Name
       </label>
-      <label htmlFor="email" className="ml-5">
-       Email: 
-     <input type="text" className="input input-bordered input-primary input-sm w-full max-w-xs" placeholder="Enter your email" />
-
+    </div>
+    <div className="md:w-2/3">
+      <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" placeholder="Enter your name"/>
+    </div>
+  </div>
+  <div className="md:flex md:items-center mb-6">
+    <div className="md:w-1/3">
+      <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4" htmlFor="inline-password">
+        Password
       </label>
+    </div>
+    <div className="md:w-2/3">
+      <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-password" type="password" placeholder="Enter your password" />
+    </div>
+  </div>
+  <div className="md:flex md:items-center mb-6">
+    <div className="md:w-1/3"></div>
+    <label className="md:w-2/3 block text-gray-500 font-bold">
+      <input className="mr-2 leading-tight" type="checkbox" />
+      <span className="text-sm">
+        Send me the newsletter!
+      </span>
+    </label>
+  </div>
+  <div className="md:flex md:items-center">
+    <div className="md:w-1/3"></div>
+    <div className="md:w-2/3">
+      <button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" type="button">
+        Sign Up
+      </button>
+    </div>
+  </div>
+</form>
 
-         <label htmlFor="product" className= "ml-5">Enter your name: </label>
-         <input type="text" placeholder="Type name here" className="input input-bordered input-primary input-sm w-full max-w-xs" />
-        <br />
-        <button className="btn btn-accent">Submit</button>
-        
 
-      </form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         </>
