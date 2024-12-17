057a355c2edeb64fd2239810657d00ba499d5359
jcas293 <jennifer.castillo48@qmail.cuny.edu>
2024-12-17 01:02:02 +0000

    final 2.2

--- app/homepage/page.tsx
+++ app/homepage/page.tsx
@@ -3,14 +3,18 @@ export default function Page() {
 
   return (
     <>
+       <h1 className = "text-3xl m-5 ml-5"> Animal Crossing New Horizons Guide </h1>
 
-        <h1 className="text-center text-xl font-bold underline m-4 text-indigo-800"> Animal Crossing Guide </h1>
-        <p className='mx-10 my-2'>This website was created by me just because 
-          I really enjoy Animal Crossing New Horzions. I wanted to give tips and tricks to 
-          all those who are starting the game.</p>
+       <img className="m-5 ml-5" src="https://assets.nintendo.com/image/upload/ar_16:9,c_lpad,w_930/b_white/f_auto/q_auto/ncom/software/switch/70010000027619/9989957eae3a6b545194c42fec2071675c34aadacd65e6b33fdfe7b3b6a86c3a" alt="Nintendo Animal Crossing Image" />
 
-          <p className='mx-10 my-2'>This website is currently a work in progress, 
-            some things will be missing but by January 2025 everything should be up and running.</p>
+
+      <h1 className="text-center text-xl font-bold underline m-4 text-indigo-800"> Animal Crossing History</h1>
+      <p className='mx-10 my-2'>The series of Animal Crossing has been around since 2001. Since then many versions for many different consoles have been released. The most recent adaptation of the game is Animal Crossing: New Horizons. In this game you take on the role of a character who moves to a deserted island after purhcasing a getaway package from Tom Nook, who is a recurring character throughout the series. Your goal is to develop the island into a thriving community by gathering resources, crafting items, decorating the landscape, and building homes. You interact with anthropomorphic animal villagers, participate in various seasonal events, and can visit other players islands via online multiplayer. The games open-ended nature allows for creativity and relaxation, with no strict objectives or time limits.</p>
+     
+      <h1 className="text-center text-xl font-bold underline m-4 text-indigo-800"> What is to come?</h1>
+      <p className='mx-10 my-2'>Eventually you will see this website will have many sections you can go to that are specific to something you might be wondering about the game. Whether it be how to get more bells, how to obtain certain items or how to earn friendship points with your villager, we will have it all! You will even be able to contribute to a public forum where everyone can add their own knowledge they have learned.</p>
+
+
 
       
 
--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,53 @@ export default function Home() {
       <>
      
         <h1 className="m-5 text-3xl"> Welcome to my Animal Crossing Guide!</h1>
+        <label className="flex cursor-pointer gap-2">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round">
+    <circle cx="12" cy="12" r="5" />
+    <path
+      d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+  </svg>
+  <input type="checkbox" value="synthwave" className="toggle theme-controller" />
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round">
+    <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+  </svg>
+</label>
 
-        <h1 className="text-center text-xl font-bold underline m-4 text-indigo-800"> Animal Crossing History</h1>
-      <p className='mx-10 my-2'>The series of Animal Crossing has been around since 2001. Since then many versions for many different consoles have been released. The most recent adaptation of the game is Animal Crossing: New Horizons. In this game you take on the role of a character who moves to a deserted island after purhcasing a getaway package from Tom Nook, who is a recurring character throughout the series. Your goal is to develop the island into a thriving community by gathering resources, crafting items, decorating the landscape, and building homes. You interact with anthropomorphic animal villagers, participate in various seasonal events, and can visit other players islands via online multiplayer. The games open-ended nature allows for creativity and relaxation, with no strict objectives or time limits.</p>
-     
-      <h1 className="text-center text-xl font-bold underline m-4 text-indigo-800"> What is to come?</h1>
-      <p className='mx-10 my-2'>Eventually you will see this website will have many sections you can go to that are specific to something you might be wondering about the game. Whether it be how to get more bells, how to obtain certain items or how to earn friendship points with your villager, we will have it all!</p>
+        <img className="m-5 ml-5" src="https://absoludicrousblog.wordpress.com/wp-content/uploads/2020/02/acnh.jpg" alt="Animal Crossing Image" />
+        
+
+        
+        <h1 className="text-center text-xl font-bold underline m-4 text-indigo-800"> Animal Crossing Guide </h1>
+        <p className='mx-10 my-2'>This website was created by me just because 
+          I really enjoy Animal Crossing New Horzions. I wanted to give tips and tricks to 
+          all those who are starting the game.</p>
+          
+
+          <h1 className="text-center text-xl font-bold underline m-4 text-indigo-800"> Under Construction! </h1>
+          <p className='mx-10 my-2'>This website is currently a work in progress, 
+            some things will be missing but by January 2025 everything should be up and running.</p>
+
+          <h1 className="text-center text-xl font-bold underline m-4 text-indigo-800"> Start Here! </h1>
+          <p className="mx-10 my-2">Please press <a href="/homepage" className="font-medium text-blue-600 underline dark:text-blue-500 hover:no-underline">here</a> to go to this websites offical homepage.</p>
+
        
         
       
